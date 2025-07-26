2d2ab6832dec479a725eea56b6af24d61a6b57ee
Balaji <81224567+balaji1610@users.noreply.github.com>
2025-07-26 18:45:14 +0530

    Add API call counting and /apiCallsCount endpoint
    Introduces tracking for the number of times each API endpoint is called and adds a new /apiCallsCount endpoint to retrieve counts. Updates all relevant routes to increment their respective counters.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const url = require("url");
 
 const exercises = require("./utils/Exercises.json");
 const list = require("./utils/Bodypartlist");
 const images = require("./utils/ImageService");
+
 const app = express();
 
 const PORT = 5000;
@@ -12,23 +14,51 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
+const apiCallsCount = [
+  {
+    apiCall: "/exercises",
+    count: 0,
+  },
+  {
+    apiCall: "/exercises/bodyPartList",
+    count: 0,
+  },
+  {
+    apiCall: "/exercises/bodyPart",
+    count: 0,
+  },
+  {
+    apiCall: "/image",
+    count: 0,
+  },
+];
+
 //status
+
+function ApiCount(call) {
+  return apiCallsCount.map((el) => {
+    return el.apiCall === call ? { ...el, count: el.count++ } : el;
+  });
+}
 
 app.get("/status", (req, res) => {
   res.status(200).send("<h1>Server is Running !!!!!</h1>");
 });
 
 app.get("/exercises", (req, res) => {
+  ApiCount(req.path);
   res.status(200).send(exercises);
 });
 
 //bodypartlist
 app.get("/exercises/bodyPartList", (req, res) => {
+  ApiCount(req.path);
   res.status(200).send(list.bodypartlist());
 });
 
 //bodyPart
 app.get("/exercises/bodyPart/:bodypart", (req, res) => {
+  ApiCount("/exercises/bodyPart");
   const getBodyPart = exercises.filter((el) => {
     return el.bodyPart === req.params.bodypart;
   });
@@ -40,6 +70,7 @@ app.get("/exercises/bodyPart/:bodypart", (req, res) => {
 });
 
 app.get("/image", (req, res) => {
+  ApiCount(req.path);
   const { exerciseId } = req.query;
   const findImageId = images
     .ImageService()
@@ -53,6 +84,20 @@ app.get("/image", (req, res) => {
     const noImage = path.join(__dirname, "./images/no-image-available.png");
     res.sendFile(noImage);
   }
+});
+
+//apiCallsCount
+app.get("/apiCallsCount", (req, res) => {
+  const totalapiCount = apiCallsCount
+    .map((el) => {
+      return el.count;
+    })
+    .reduce((prev, curr) => {
+      return prev + curr;
+    }, 0);
+  const totalCount = [...apiCallsCount, { totalapiCounts: totalapiCount }];
+
+  res.status(200).send(totalCount);
 });
 
 app.listen(PORT, () => {
